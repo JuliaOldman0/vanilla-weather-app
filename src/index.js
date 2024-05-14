bfcec0e42c8e6d0ec4d10ec4d31d629423df32ea
JuliaOldman0 <julia.oldman0@gmail.com>
2024-05-14 17:44:12 -0400

    added the date and time

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
 function refreshWeather(response) {
-let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.main.temp;
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let speedElement = document.querySelector("#speed");
-let timeElement = document.querySelector("#time");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.dt * 1000);
 
-
-console.log(response.data);
-
-cityElement.innerHTML = response.data.name;
-timeElement.innerHTML = "Tuesday 14:50";
-humidityElement.innerHTML = `${response.data.main.humidity}%`;
-speedElement.innerHTML = `${response.data.wind.speed}km/h`;
-descriptionElement.innerHTML = response.data.weather[0].description;
-temperatureElement.innerHTML = Math.round(temperature);
-
+  cityElement.innerHTML = response.data.name;
+  timeElement.innerHTML = formatDate(date);
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = day[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
-let apiKey = "e42b7ace2f914435cbe3a5b48484eb18";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "e42b7ace2f914435cbe3a5b48484eb18";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-
 function handleSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 
-searchCity(searchInput.value);
-
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
