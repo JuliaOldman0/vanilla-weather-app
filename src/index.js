abffe30a8655fc32dd1fd5cb193b9c767e62d4cf
JuliaOldman0 <julia.oldman0@gmail.com>
2024-05-15 14:11:52 -0400

    finished weather app

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.name;
   timeElement.innerHTML = formatDate(date);
@@ -14,10 +15,46 @@ function refreshWeather(response) {
   speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   descriptionElement.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = WeatherIcon(response.data.weather[0].icon);
+}
+
+function WeatherIcon(icon) {
+  const codeMapping = {
+    "01d": "fas fa-sun",
+    "01n": "fas fa-sun",
+
+    "02d": "fas fa-cloud-sun",
+    "02n": "fas fa-cloud-sun",
+
+    "03d": "fas fa-cloud-sun",
+    "03n": "fas fa-cloud-sun",
+
+    "04d": "fas fa-cloud-rain",
+    "04n": "fas fa-cloud-rain",
+
+    "09d": "fas fa-cloud-sun-rain",
+    "09n": "fas fa-cloud-sun-rain",
+
+    "10d": "fas fa-cloud-showers-heavy",
+    "10n": "fas fa-cloud-showers-heavy",
+
+    "11d": "fas fa-bolt",
+    "11n": "fas fa-bolt",
+
+    "13d": "fas fa-wind",
+    "13n": "fas fa-wind",
+
+    "50d": "fas fa-water",
+    "50n": "fas fa-water",
+  };
+
+  const iconClass = codeMapping[icon];
+  if (iconClass) {
+    return `<i class="${iconClass}"></i>`;
+  }
 }
 
 function formatDate(date) {
-  
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
@@ -29,13 +66,13 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
-  let day = day[date.getDay()];
-
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   return `${day} ${hours}:${minutes}`;
 }
 
